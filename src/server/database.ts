a2f96b8353339e7fe959430ed3001ad613c083e6
lucas-barake <86905052+lucas-barake@users.noreply.github.com>
2024-01-25 17:18:12 -0500

    chore: singleton db client

--- src/server/database.ts
+++ src/server/database.ts
@@ -4,27 +4,35 @@ import { type DB } from "kysely-codegen";
 import { env } from "@/env";
 import pc from "picocolors";
 
+const globalForDb = globalThis as unknown as { db: Kysely<DB> | undefined };
+
 const dialect = new PostgresDialect({
   pool: new Pool({
     connectionString: env.DATABASE_URL,
   }),
 });
 
-export const db = new Kysely<DB>({
-  dialect,
-  log:
-    env.NODE_ENV === "development"
-      ? (event) => {
-          const formattedSql = event.query.sql.replace(/\$(\d+)/g, (_, index) => {
-            const param = event.query.parameters[Number(index) - 1];
-            return param !== null && param !== undefined && typeof param.toString === "function"
-              ? // eslint-disable-next-line @typescript-eslint/no-base-to-string
-                pc.green(param.toString())
-              : pc.red("UNKNOWN");
-          });
+function createDbClient(): Kysely<DB> {
+  return new Kysely<DB>({
+    dialect,
+    log:
+      env.NODE_ENV === "development"
+        ? (event) => {
+            const formattedSql = event.query.sql.replace(/\$(\d+)/g, (_, index) => {
+              const param = event.query.parameters[Number(index) - 1];
+              return param !== null && param !== undefined && typeof param.toString === "function"
+                ? // eslint-disable-next-line @typescript-eslint/no-base-to-string
+                  pc.green(param.toString())
+                : pc.red("UNKNOWN");
+            });
 
-          const kyselyLabel = pc.yellow(`[Kysely (${event.queryDurationMillis.toFixed(2)}ms)]`);
-          console.debug(`\n${kyselyLabel}\n${formattedSql}\n`);
-        }
-      : undefined,
-});
+            const kyselyLabel = pc.yellow(`[Kysely (${event.queryDurationMillis.toFixed(2)}ms)]`);
+            console.debug(`\n${kyselyLabel}\n${formattedSql}\n`);
+          }
+        : undefined,
+  });
+}
+
+export const db = globalForDb.db ?? createDbClient();
+
+if (env.NODE_ENV !== "production") globalForDb.db = db;
